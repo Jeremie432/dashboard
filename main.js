c0e687f2f39929890080749066eb41ab96c1fe9d
Jeremie432 <94252669+Jeremie432@users.noreply.github.com>
2022-12-14 00:20:16 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+import ScoreboardView from "./scoreboard/ScoreboardView.js";
+
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let playerThreeScore = 0;
+let playerFourScore = 0;
+let playerFiveScore = 0;
+let playerSixScore = 0;
+
+
+const root = document.querySelector("#app");
+const view = new ScoreboardView(root, "Team A", "Team B", "Team C", "Team D", "Team E", "Team F", (player, direction) => {
+	const difference = direction === "minus" ? -1 : 1;
+
+	if (player === "one") {
+		playerOneScore = Math.max(playerOneScore + difference, 0);
+	} 
+
+	if (player === "two") {
+		playerTwoScore = Math.max(playerTwoScore + difference, 0);
+	} 
+
+	if (player === "three") {
+		playerThreeScore = Math.max(playerThreeScore + difference, 0);
+	} 
+
+	if (player === "four") {
+		playerFourScore = Math.max(playerFourScore + difference, 0);
+	} 
+
+	if (player === "five") {
+		playerFiveScore = Math.max(playerFiveScore + difference, 0);
+	} 
+	
+	if (player === "six") {
+		playerSixScore = Math.max(playerSixScore + difference, 0);
+	} 
+	
+
+	view.update(playerOneScore, playerTwoScore, playerThreeScore, playerFourScore, playerFiveScore, playerSixScore);
+});
+
